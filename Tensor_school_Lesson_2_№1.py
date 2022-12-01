3cd7c309bfc198b7f755650578c747c974138fa7
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-01 19:42:33 +0300

    Add files via upload

---
+++ Tensor_school_Lesson_2_№1.py
@@ -0,0 +1,3 @@
+first_count=float(input("Введите число: "))
+second_count=float(input("Введите число: "))
+print(f"a + b =", first_count+second_count, "a - b =", first_count-second_count, "a * b =", first_count*second_count, "a / b =", first_count/second_count, "a ^ b =", first_count**second_count, "a % b =", first_count%second_count, "a // b =", first_count//second_count)
